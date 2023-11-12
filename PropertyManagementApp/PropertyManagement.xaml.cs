13022d6abb9685654f14d58bd9d67f4ff666d1a9
mouhssin <mouhssin@ALPHA>
2023-11-12 00:53:04 -0500

    Classes "Agent, Property, PropertyManagement"  generated from Database

---
+++ PropertyManagementApp/PropertyManagement.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PropertyManagementApp
+{
+    /// <summary>
+    /// Interaction logic for PropertyManagement.xaml
+    /// </summary>
+    public partial class PropertyManagement : Window
+    {
+        public PropertyManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
