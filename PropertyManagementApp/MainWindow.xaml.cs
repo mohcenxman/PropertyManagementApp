b12bc95975969c390f483b72b4e607a97469f34a
mouhssin <mouhssin@ALPHA>
2023-11-12 01:46:22 -0500

    property management UI created Ok and Cancel buttons binded and working

--- PropertyManagementApp/MainWindow.xaml.cs
+++ PropertyManagementApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,80 @@ namespace PropertyManagementApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        PropertyManagementEntities manage;
+        bool open = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            //txtUser.DataContext = manage.Agents.ToList();
+            manage = new PropertyManagementEntities();
+
+            // Check if the entered username and password are valid
+            string enteredUsername = txtUser.Text;
+            string enteredPassword = txtPassword.Password;
+
+            var agent = manage.Agents.FirstOrDefault(a => a.UserName == enteredUsername && a.Password == enteredPassword);
+
+            if (agent != null)
+            {
+                // Login successful
+                open = true;
+
+                // Open PropertyManagementWindow
+                PropertyManagement propertyManagement = new PropertyManagement();
+                propertyManagement.Show();
+
+                // Close the current login window
+                this.Close();
+            }
+            else
+            {
+                // Invalid username or password
+                MessageBox.Show("Invalid username or password. Please try again.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUser_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (txtUser.Text != null)
+            {
+                btnOk.IsEnabled = true;
+            }
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password != null)
+            {
+                btnOk.IsEnabled = true;
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (open)
+            {
+                // Confirmation pour quitter l'application
+                if (MessageBox.Show("Continue to colse the app ?", "Attention !",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
         }
     }
 }
